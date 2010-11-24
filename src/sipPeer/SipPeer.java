53323b8e1ca3f5b514b648bd6b99d341720c0f95
Andreas <hamburger1984@gmail.com>
2010-11-24 12:18:51 +0100

    Inspirationen aus dem textclient

---
+++ src/sipPeer/SipPeer.java
@@ -0,0 +1,141 @@
+package sipPeer;
+
+import java.net.*;
+import java.util.*;
+//import javax.sip.*;
+import javax.swing.*;
+import javax.swing.text.*;
+import java.awt.*;
+import java.awt.event.*;
+import dev2dev.textclient.*;
+
+public class SipPeer 
+	extends JFrame
+	implements MessageProcessor,
+		ActionListener{
+	
+	public static void main(String[] args){
+		System.out.println("hallo welt");
+
+		if(args.length!=2){
+			System.out.println("usage: java -jar SipPeer.jar username port");
+			System.exit(0);
+		}
+		try{
+			String username = args[0];
+			int port = Integer.parseInt(args[1]);
+			String ip = InetAddress.getLocalHost().getHostAddress();
+
+			SipLayer sipLayer = new SipLayer(username, ip, port);
+			
+			SipPeer app = new SipPeer(sipLayer);
+			app.show();
+		} catch(Exception ex){
+			ex.printStackTrace();
+		}		
+	}
+
+	private SipLayer sipLayer;
+	private JTextField username;
+	private JButton applyUsernamePort;
+	private JTextPane log;
+	private StyledDocument logdoc;
+
+	public SipPeer(SipLayer sipLayer){
+		super("SipPeer");
+		
+		addWindowListener(new WindowAdapter(){
+			public void windowClosing(WindowEvent e){
+				e.getWindow().dispose();
+				System.exit(0);
+			}		
+		});
+
+		this.sipLayer = sipLayer;
+		this.sipLayer.setMessageProcessor(this);
+
+		initUI();
+	}
+
+	private void initUI(){
+		GridBagLayout grid = new GridBagLayout();
+		GridBagConstraints c = new GridBagConstraints();
+		c.gridx = c.gridy = 0;
+		c.gridwidth = c.gridheight = 1;
+		c.fill = GridBagConstraints.HORIZONTAL;
+		c.ipadx = c.ipady = 4;
+
+		setLayout(grid);
+
+		c.gridwidth=3;
+		JLabel label = new JLabel("Address: " + sipLayer.getHost() + ":" + String.valueOf(sipLayer.getPort()));
+		grid.setConstraints(label, c);
+		add(label);
+
+		c.gridwidth = 1;
+		c.gridy = 1;
+		c.gridx = 0;
+		label = new JLabel("username:");
+		grid.setConstraints(label, c);
+		add(label);
+
+		c.gridx = 1;
+		username = new JTextField(sipLayer.getUsername());
+		grid.setConstraints(username, c);
+		add(username);
+
+		c.gridx = 2;
+		applyUsernamePort = new JButton("Apply");
+		grid.setConstraints(applyUsernamePort, c);
+		add(applyUsernamePort);
+
+		c.gridx = 0;
+		c.gridy = 2;
+		c.gridwidth = 3;
+		c.weighty = 1;
+		c.fill = GridBagConstraints.BOTH;
+		log = new JTextPane();
+		logdoc = log.getStyledDocument();
+		JScrollPane logscroll = new JScrollPane(log);
+		logscroll.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+		grid.setConstraints(logscroll, c);
+		add(logscroll);
+	
+		c.fill = GridBagConstraints.HORIZONTAL;
+		c.weighty = 0;
+
+
+		pack();
+		setVisible(true);
+	}
+
+	private void appendToLog(String message){
+		try{
+			logdoc.insertString(logdoc.getLength(), "msg: " + message + "\n", null); 
+		} catch(BadLocationException ex){
+			ex.printStackTrace();
+		}
+	}
+
+// -------------- MessageProcessor
+	public void processMessage(String sender, String message){
+		appendToLog("msg: " + message + "\n");
+	}
+
+	public void processError(String errorMessage){
+		appendToLog("err: " + errorMessage + "\n"); 
+	}
+
+	public void processInfo(String infoMessage){
+		appendToLog("info: " + infoMessage + "\n");
+	}	
+
+// --------------- ActionListener
+	public void actionPerformed(ActionEvent e){
+		Object source = e.getSource();
+		if(source==applyUsernamePort){
+			sipLayer.setUsername(username.getText());
+		}
+	}
+
+}
